ab486d03cf51466591fce174d9ad360720b3f347
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-01-16 15:18:18 +0200

    ALX_Simple_Quiz

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer( correctAnser ="4") {
+    // Function body
+}
+
+document.querySelector (name= "quiz")
+
+let userAnswer = 4
+
+console.log(userAnswer);
+
+if (4) {
+    "Correct! Well done."
+} else {
+    "That's Incorrect. Try again"
+}
+
+document.getElementById("submit-answer")
+
+button.addEventsListner('click', function(checkAnswer) {alert ('button clicked!');});
