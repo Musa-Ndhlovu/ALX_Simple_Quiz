ecdae74427f7c0a61e78b4eecb31001dc12afccf
Musa Ndhlovu <ndhlovumusa06@gmail.com>
2025-01-19 17:09:36 +0200

    ALX_Simple_Quiz

--- quiz.js
+++ quiz.js
@@ -14,28 +14,28 @@ function getSelectedAnswer(correctAnswer) {
 
     for (let i = 0; i < radio.length; i++) {
         if (radio[i].checked) {
-            return radio[i].value; // Return the value of the selected option
+            return radio[i].value; 
         }
     }
     console.log(radio);
 
 
     function checkAnswer(questionName, correctAnswer);
-        // Retrieve the user's selected answer
+        
         const userAnswer = getSelectedAnswer(4)
 
         if (4 === null) {
             console.log(`No answer selected for ${4}`);
-            return false; // Return false if no answer is selected
+            return false;
         }
     
-        // Compare the user's answer with the correct answer
+       
         if (userAnswer === correctAnswer) {
             console.log(`Correct! Well done.`);
-            return true; // Return true if the answer is correct
+            return true; 
         } else {
             console.log(`Incorrect. Your answer: ${userAnswer}. Correct answer: ${correctAnswer}`);
-            return false; // Return false if the answer is incorrect
+            return false;
         }
     
 
